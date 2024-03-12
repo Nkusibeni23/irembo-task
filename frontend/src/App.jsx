44b3b4297fa1e0b62928c720ff2cb507574d9b6a
Your GitHub Username <Your GitHub Email>
2024-03-12 16:55:36 +0200

    backend for email

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import axios from "axios";
 import BusinessDetails from "./components/BusinessDetails";
 import BusinessOwnerDetails from "./components/BusinessOwnerDeatils";
 import ProductionInformation from "./components/ProductionInformation";
@@ -6,8 +7,22 @@ import ProductionInformation from "./components/ProductionInformation";
 const App = () => {
   const [formData, setFormData] = useState({});
 
-  const handleFormSubmit = () => {
-    console.log("Form Data Submitted:", formData);
+  const handleFormSubmit = async () => {
+    try {
+      // Make a POST request to your backend API
+      const response = await axios.post(
+        "http://localhost:3003/api/user/signup",
+        formData
+      );
+
+      // Log the response from the backend
+      console.log("Response from the backend:", response.data);
+
+      // You can handle the response or update your UI accordingly
+    } catch (error) {
+      // Handle errors if any
+      console.error("Error submitting form:", error.message);
+    }
   };
 
   return (
